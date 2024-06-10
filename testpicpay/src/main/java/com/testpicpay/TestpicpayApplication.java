ce2c4aacdab76a8b6452b6f57e4624021d5fcde7
marcos3777 <marcos_vinicius_11@hotmail.com>
2024-06-10 12:10:18 -0300

    feat: initial config

---
+++ testpicpay/src/main/java/com/testpicpay/TestpicpayApplication.java
@@ -0,0 +1,13 @@
+package com.testpicpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestpicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestpicpayApplication.class, args);
+	}
+
+}
---
+++ testpicpay/src/test/java/com/testpicpay/TestpicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testpicpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestpicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
